0fd33f7d0b8613840f15c26022ce742173a483aa
toomanyMG <tchiki.tshiki@gmail.com>
2023-02-01 20:46:04 +0100

    Created the crud for the bunker

---
+++ src/Controller/BunkerController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Bunker;
+use App\Form\BunkerType;
+use App\Repository\BunkerRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/bunker')]
+class BunkerController extends AbstractController
+{
+    #[Route('/', name: 'app_bunker_index', methods: ['GET'])]
+    public function index(BunkerRepository $bunkerRepository): Response
+    {
+        return $this->render('bunker/index.html.twig', [
+            'bunkers' => $bunkerRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_bunker_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, BunkerRepository $bunkerRepository): Response
+    {
+        $bunker = new Bunker();
+        $form = $this->createForm(BunkerType::class, $bunker);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $bunkerRepository->save($bunker, true);
+
+            return $this->redirectToRoute('app_bunker_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('bunker/new.html.twig', [
+            'bunker' => $bunker,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_bunker_show', methods: ['GET'])]
+    public function show(Bunker $bunker): Response
+    {
+        return $this->render('bunker/show.html.twig', [
+            'bunker' => $bunker,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_bunker_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Bunker $bunker, BunkerRepository $bunkerRepository): Response
+    {
+        $form = $this->createForm(BunkerType::class, $bunker);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $bunkerRepository->save($bunker, true);
+
+            return $this->redirectToRoute('app_bunker_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('bunker/edit.html.twig', [
+            'bunker' => $bunker,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_bunker_delete', methods: ['POST'])]
+    public function delete(Request $request, Bunker $bunker, BunkerRepository $bunkerRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$bunker->getId(), $request->request->get('_token'))) {
+            $bunkerRepository->remove($bunker, true);
+        }
+
+        return $this->redirectToRoute('app_bunker_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
