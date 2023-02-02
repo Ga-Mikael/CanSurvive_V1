f324a5db233efd83d9c012d8f50912c1aad251dd
toomanyMG <tchiki.tshiki@gmail.com>
2023-02-02 09:44:49 +0100

    US04 Can Entity-CRUD-Front

--- src/Form/CanType.php
+++ src/Form/CanType.php
@@ -6,16 +6,19 @@ use App\Entity\Can;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class CanType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('capacity')
-            ->add('expirationDate')
-            ->add('barCode')
+            ->add('name', TextType::class, ['attr' => ['placeholder' => 'Nom']])
+            ->add('capacity', IntegerType::class, ['attr' => ['placeholder' => 'Taille (1 ou 2)']])
+            ->add('expirationDate', DateType::class, ['attr' => ['placeholder' => 'date de péremption'], 'widget' => 'single_text'])
+            ->add('barCode', IntegerType::class, ['attr' => ['placeholder' => 'Code Barre']])
         ;
     }
 
