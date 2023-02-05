1b1ec1894fe8f0b8a5264512d9d36131c670d8e4
toomanyMG <tchiki.tshiki@gmail.com>
2023-02-05 21:29:59 +0100

    new features : calculation of the period of surviving, info of the stock and calculation of the comsumption of everyone

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,8 @@
 
 namespace App\Controller;
 
-
+use App\Repository\CanRepository;
+use App\Service\BunkerManager;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -13,14 +14,21 @@ class HomeController extends AbstractController
 {
 
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(BunkerManager $bunkerManager): Response
     {
         /**  @var \App\Entity\User */
         $user = $this->getUser();
         $bunker = $user->getBunker();
 
+        $totalCurrentStock = $bunkerManager->getAllCan($bunker);
+        $totalComsumption = $bunkerManager->getResidentDailyComsumption($bunker, $user);
+        $dayBeforeEnd = $totalCurrentStock / $totalComsumption;
+
         return $this->render('home/index.html.twig', [
             'bunker' => $bunker,
+            'totalComsumption' => $bunkerManager->getResidentDailyComsumption($bunker, $user),
+            'dayBeforeEnd' => $dayBeforeEnd,
+            'canStock' => $bunkerManager->getAllCan($bunker),
 
         ]);
     }
